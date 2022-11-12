f7d0150709a6c01b31a7222c9d0c3ae42f01bf91
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-11-11 21:57:22 -0800

    I can't breathe

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Pokedex</h1>
+    </div>
+  );
+}
+
+export default App;
