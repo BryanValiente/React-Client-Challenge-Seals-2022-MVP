59dc2de0a4d5bfeb67c14a83890300b394010763
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-11-11 22:09:47 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Header from './components/Header/header.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <h1>Pokedex</h1>
     </div>
   );
